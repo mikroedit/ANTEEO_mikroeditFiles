a5cfb319b23b37f3a279ca03b866392be40fd4f8
mikroedit <mikro.szweda2005@gmail.com>
2024-09-03 15:06:07 +0200

    dzialaaa

---
+++ python-project3/project3.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+# Twój klucz API Baselinker
+api_key = '5005339-5019417-QRJNYQEGHEDF69PWCJ2SE9JA2OLTJOS74S7IPVGFTIKYAEB8RGK3N2L2NPC5X41L'
+
+# URL endpointu API
+url = 'https://api.baselinker.com/connector.php'
+
+# Parametry w formacie JSON
+parameters = {
+    'product_id': '100967406'     # ID produktu do usunięcia
+}
+
+# Dane do wysłania w żądaniu POST
+data = {
+    'token': api_key,
+    'method': 'deleteInventoryProduct',
+    'parameters': json.dumps(parameters)
+}
+
+# Wysyłanie żądania POST
+response = requests.post(url, data=data)
+
+# Sprawdzanie odpowiedzi
+if response.status_code == 200:
+    print('Sukces:', response.json())
+else:
+    print('Błąd:', response.status_code, response.text)
